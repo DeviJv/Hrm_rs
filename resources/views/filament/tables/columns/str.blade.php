46a599dcdc31f7e866189a12d842ccb6af2bd47b
DeviJv <septian.ardi.wijaya@gmail.com>
2024-10-07 15:25:15 +0700

    Fixed Download Item from table resource
    Fixed Download Item from table resource

--- resources/views/filament/tables/columns/str.blade.php
+++ resources/views/filament/tables/columns/str.blade.php
@@ -1,3 +1,4 @@
 <div class="fi-ta-text-item-label text-sm leading-6 text-gray-950 dark:text-white underline">
-    <a href="{{ asset($getState()) }}" __new>Unduh</a>
+    <a href="{{ asset('storage/' . $getState()) }}" target="__new">Unduh</a>
+
 </div>
