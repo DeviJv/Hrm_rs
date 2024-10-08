677a31516bf95fbfe5df9f67f018ff5e00f33e5d
DeviJv <septian.ardi.wijaya@gmail.com>
2024-10-08 18:28:33 +0700

    added default state every download doc
    added default state every download doc

--- resources/views/filament/tables/columns/str.blade.php
+++ resources/views/filament/tables/columns/str.blade.php
@@ -1,4 +1,9 @@
-<div class="fi-ta-text-item-label text-sm leading-6 text-gray-950 dark:text-white underline">
-    <a href="{{ asset('storage/' . $getState()) }}" target="__new">Unduh</a>
-
-</div>
+@if ($getState() !== null)
+    <div class="fi-ta-text-item-label text-sm leading-6 text-gray-950 dark:text-white underline">
+        <a href="{{ asset('storage/' . $getState()) }}" target="__new">Unduh</a>
+    </div>
+@else
+    <div>
+        -
+    </div>
+@endif
