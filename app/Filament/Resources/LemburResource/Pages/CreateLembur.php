b27e36fb1ff119954499ace0ddff2e2c78271df8
DeviJv <septian.ardi.wijaya@gmail.com>
2024-10-08 13:22:45 +0700

    Update CreateLembur.php

--- app/Filament/Resources/LemburResource/Pages/CreateLembur.php
+++ app/Filament/Resources/LemburResource/Pages/CreateLembur.php
@@ -37,17 +37,17 @@ class CreateLembur extends CreateRecord
         $data = $this->data;
         $session_name = auth()->user()->name;
         $karyawan = Karyawan::where('id', $data['karyawan_id'])->first();
-        $check_tgl = Lembur::where('karyawan_id', $data['karyawan_id'])
-            ->whereDate('tgl_lembur', '=', $data['tgl_lembur'])->count();
-        if ($check_tgl > 0) {
-            Notification::make()
-                ->danger()
-                ->title("Ops <b>{$session_name}</b> input lembur gagal Gagal!")
-                ->body("Mohon maaf data karyawan <b>{$karyawan->nama}</b> pada tanggal {$data['tgl_lembur']} sudah di input!<br> pilih karyawan
-                lainnya atau ubah tanggal lembur atau klik batal untuk membatalkan.")
-                ->send();
-            $this->halt();
-        }
+        // $check_tgl = Lembur::where('karyawan_id', $data['karyawan_id'])
+        //     ->whereDate('tgl_lembur', '=', $data['tgl_lembur'])->count();
+        // if ($check_tgl > 0) {
+        //     Notification::make()
+        //         ->danger()
+        //         ->title("Ops <b>{$session_name}</b> input lembur gagal Gagal!")
+        //         ->body("Mohon maaf data karyawan <b>{$karyawan->nama}</b> pada tanggal {$data['tgl_lembur']} sudah di input!<br> pilih karyawan
+        //         lainnya atau ubah tanggal lembur atau klik batal untuk membatalkan.")
+        //         ->send();
+        //     $this->halt();
+        // }
         // Runs before the form fields are saved to the database.
     }
 
