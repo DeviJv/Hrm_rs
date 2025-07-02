77645b7b401181f7cb8536519be02a9dac7b72f5
DeviJv <septian.ardi.wijaya@gmail.com>
2025-07-02 10:52:18 +0700

    Coba
    Coba

--- app/Filament/Clusters/DashboardCluster.php
+++ app/Filament/Clusters/DashboardCluster.php
@@ -3,7 +3,10 @@
 namespace App\Filament\Clusters;
 
 use Filament\Clusters\Cluster;
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 
 class DashboardCluster extends Cluster {
+
     protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+    use HasPageShield;
 }
